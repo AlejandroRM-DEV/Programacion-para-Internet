2dec14b4c6acc945feb299456b69c2f45278d475
Alejandro Ramírez Muñoz <alejandrorm45@yahoo.com>
2016-07-05 18:10:55 -0500

    Terminado en windows

--- ListadoFTP/main.cpp
+++ ListadoFTP/main.cpp
@@ -12,120 +12,31 @@
 #include "URLParser.h"
 
 #define TAMANO_BUFFER 512
+#define NIVEL_PROFUNDIDAD 3
 
 using namespace std;
+
+static bool LOG = false;
 
 #define C_STR(ITEMS)  ( ( dynamic_cast<ostringstream &> ( ostringstream() . seekp( 0, ios_base::cur ) << ITEMS ) ) \
                        .str().c_str() )
 
 template <typename T>
-inline bool str_to_num( const std::string& str, T &num ) {
-    std::istringstream iss( str );
+inline bool str_to_num( const string& str, T &num ) {
+    istringstream iss( str );
     return ( iss >> num ) ? true : false;
 }
 
 bool inputError();
 URL* obtenerURL();
-int recibir( SocketPortable &control, string &copiaDescripcion );
+int recibir( SocketPortable &socket, string &copia );
 int recibir( SocketPortable &control );
-int listarDirectorios( SocketPortable &control, int profundidad,  stringstream &listado );
-
-int listarDirectorios( SocketPortable &control, int profundidad, stringstream &listado ) {
-    string descripcion, str, ip, aux, nombre;
-    uint16_t puerto, temp;
-    SocketPortable datos;
-
-    if( control.send( "PASV\r\n", 6, 0 ) < 0) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control, descripcion ) <= 0 ) return -1;
-
-    size_t inicio = descripcion.find_first_of( "(", 0 ) + 1;
-    stringstream ss( descripcion.substr( inicio, descripcion.find_first_of( ")", 0 ) - inicio ) );
-
-    getline( ss, str, ',' );
-    ip = str + ".";
-    getline( ss, str, ',' );
-    ip += str + ".";
-    getline( ss, str, ',' );
-    ip += str + ".";
-    getline( ss, str, ',' );
-    ip += str;
-
-    getline( ss, str, ',' );
-    str_to_num( str, puerto );
-    getline( ss, str );
-    str_to_num( str, temp );
-    puerto = ( puerto * 256 ) + temp;
-
-    struct addrinfo hints;
-    /*  Connection Establishment  */
-    memset( &hints, 0, sizeof ( addrinfo ) );
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    if( !datos.connect( ip.c_str(), C_STR( puerto ), &hints ) ) {
-        cout << datos.getLastErrorMessage() << endl;
-        return -1;
-    }
-
-    if( control.send( "LIST\r\n", 6, 0 )< 0 ) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control ) <= 0 ) return -1;
-
-    descripcion = "";
-    if( recibir( datos, descripcion ) < 0 ) return -1;
-    datos.close();
-    if( recibir( control ) <= 0 ) return -1;
-
-    stringstream ss_2( descripcion );
-    while( getline( ss_2, str ) ) {
-        istringstream fila( str );
-        fila >> aux >> aux >> aux >> aux >> aux >> aux >> aux >> aux >> nombre;
-        for( int i = 0, j = profundidad + 1; i < j; i++ ) {
-            listado << "    |";
-        }
-        if( str.at( 0 ) != 'd' ) {
-            listado << "---> " << nombre << endl;
-        } else {
-            listado << "---[ " << nombre << endl;
-
-            if( profundidad < 2 ) {
-                if( control.send( "PWD\r\n", 5, 0 )< 0 ) {
-                    cout << control.getLastErrorMessage() << endl;
-                    return -1;
-                }
-                if( recibir( control, aux ) <= 0 ) return -1;
-                aux.erase( 0, aux.find_first_of( "\"" ) + 1 );
-                aux.erase( aux.find_first_of( "\"" ) );
-
-                string send = C_STR( "CWD " << aux << ( aux.back() == '/' ? "" : "/" ) << nombre << "\r\n" );
-                if( control.send( send.c_str(), send.length(), 0 )< 0 ) {
-                    cout << control.getLastErrorMessage() << endl;
-                    return -1;
-                }
-                if( recibir( control ) <= 0 ) return -1;
-
-                listarDirectorios( control, profundidad + 1, listado );
-
-                if( control.send( "CDUP\r\n", 6, 0 )< 0 ) {
-                    cout << control.getLastErrorMessage() << endl;
-                    return -1;
-                }
-                if( recibir( control ) <= 0 ) return -1;
-            }
-        }
-    }
-    for( int i = 0, j = profundidad; i < j; i++ ) {
-        listado << "    |";
-    }
-    listado << endl;
-    return 0;
-}
+int listarDirectorios( SocketPortable &control, int profundidad,  stringstream &listado, string padding ) throw ( int );
 
 int main() {
+    stringstream listado;
+    string comando;
+    int codigo;
     struct addrinfo hints;
     SocketPortable control;
 
@@ -133,43 +44,41 @@ int main() {
     if ( url == nullptr ) {
         return -1;
     }
-    /*  Connection Establishment  */
+
     memset( &hints, 0, sizeof ( addrinfo ) );
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    if( !control.connect( url->host.c_str(), url->puerto.c_str(), &hints ) ) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
+    try {
+        if( !control.connect( url->host.c_str(), url->puerto.c_str(), &hints ) ) throw - 1;
+        if( !( codigo = recibir( control ) ) ) throw codigo;
+
+        comando = C_STR( "USER " << url->usuario << "\r\n" );
+        if( !( codigo = control.send( comando.c_str(), comando.length(), 0 ) ) ) throw codigo;
+        if( !( codigo = recibir( control ) ) ) throw codigo;
+
+        comando = C_STR( "PASS " << url->contrasena << "\r\n" );
+        if( !( codigo = control.send( comando.c_str(), comando.length(), 0 ) ) )throw codigo;
+        if( !( codigo = recibir( control ) ) ) throw codigo;
+
+        if( !( codigo = control.send( "CWD /\r\n", 7, 0 ) ) ) throw codigo;
+        if( !( codigo = recibir( control ) ) ) throw codigo;
+
+        listado << "---| Root" << endl;
+        listarDirectorios( control, 0, listado, "   |" );
+        cout << listado.str() << endl;
+
+        if( !( codigo = control.send( "QUIT\r\n", 6, 0 ) ) ) throw codigo;
+        if( !( codigo = recibir( control ) ) ) throw codigo;
+    } catch( int e ) {
+        if( e == -1 ) {
+            cout << control.getLastErrorMessage() << endl;
+            control.close();
+            return -1;
+        } else if( e == 0 ) {
+            cout << "Conexion cerrada" << endl;
+            control.close();
+        }
     }
-    if( recibir( control ) <= 0 ) return -1;
-    /*  Usuario  */
-    if( control.send( C_STR( "USER " << url->usuario << "\r\n" ), 7 + url->usuario.length(), 0 ) < 0  ) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control ) <= 0 ) return -1;
-    /*  Contraseña  */
-    if( control.send( C_STR( "PASS " << url->contrasena << "\r\n" ), 7 + url->contrasena.length(), 0 ) < 0  ) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control ) <= 0 ) return -1;
-    /*  Listado  */
-    if( control.send( "CWD /\r\n", 7, 0 ) < 0) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control ) <= 0 ) return -1;
-    stringstream listado;
-    listado << "---| Root" << endl;
-    listarDirectorios( control, 0, listado );
-    cout << listado.str() << endl;
-    /*  Salir  */
-    if( control.send( "QUIT\r\n", 6, 0 ) < 0 ) {
-        cout << control.getLastErrorMessage() << endl;
-        return -1;
-    }
-    if( recibir( control ) <= 0 ) return -1;
 
     return 0;
 }
@@ -209,32 +118,116 @@ URL* obtenerURL() {
 }
 
 int recibir( SocketPortable &control ) {
-    string nulo;
-    return recibir( control, nulo );
+    string respuesta;
+    int resultado = recibir( control, respuesta );
+    if( LOG ) cout << respuesta;
+    return resultado;
 }
 
-int recibir( SocketPortable &control, string &copiaDescripcion ) {
+int recibir( SocketPortable &socket, string &copia ) {
     char *buffer = new char[TAMANO_BUFFER + 1];
-    int codigo, res;
+    int resultado;
 
+    copia = "";
     do {
-        res = control.recv( buffer, TAMANO_BUFFER, 0 );
-        if( res == 0 ) {
-            //cout << "Conexion cerrada" << endl;
-            codigo = 0;
-        } else if ( res < 0 ) {
-            cout << control.getLastErrorMessage() << endl;
-            codigo = -1;
+        resultado = socket.recv( buffer, TAMANO_BUFFER, 0 );
+        if( resultado == 0 ) {
+            return 0;
+        } else if ( resultado < 0 ) {
+            return -1;
         } else {
-            buffer[res] = 0;
-            //cout << buffer;
-            copiaDescripcion += buffer;
+            buffer[resultado] = 0;
+            copia += buffer;
         }
-    } while( res == TAMANO_BUFFER );
-    str_to_num( C_STR( copiaDescripcion.at( 0 ) << copiaDescripcion.at( 1 ) << copiaDescripcion.at( 2 ) ), codigo );
+    } while( resultado == TAMANO_BUFFER );
+    str_to_num( C_STR( copia.at( 0 ) << copia.at( 1 ) << copia.at( 2 ) ), resultado );
 
     delete buffer;
-    return codigo;
+    return resultado;
+}
+
+int listarDirectorios( SocketPortable &control, int profundidad, stringstream &listado, string padding )throw ( int ) {
+    stringstream ssPasivo, ssListado;
+    string aux, aux_2, ip, nombre;
+    SocketPortable datos;
+    struct addrinfo hints;
+    uint16_t puerto, temp;
+    size_t inicio;
+    int codigo;
+
+    if( !( codigo = control.send( "PASV\r\n", 6, 0 ) ) ) throw codigo;
+    if( !( codigo = recibir( control, aux ) ) ) throw codigo;
+    if( LOG ) cout << aux;
+
+    inicio = aux.find_first_of( "(", 0 ) + 1;
+    ssPasivo << aux.substr( inicio, aux.find_first_of( ")", 0 ) - inicio );
+
+    getline( ssPasivo, aux_2, ',' );
+    ip = aux_2 + ".";
+    getline( ssPasivo, aux_2, ',' );
+    ip += aux_2 + ".";
+    getline( ssPasivo, aux_2, ',' );
+    ip += aux_2 + ".";
+    getline( ssPasivo, aux_2, ',' );
+    ip += aux_2;
+
+    getline( ssPasivo, aux_2, ',' );
+    str_to_num( aux_2, puerto );
+    getline( ssPasivo, aux_2 );
+    str_to_num( aux_2, temp );
+    puerto = ( puerto * 256 ) + temp;
+
+    memset( &hints, 0, sizeof ( addrinfo ) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if( !datos.connect( ip.c_str(), C_STR( puerto ), &hints ) ) {
+        cout << datos.getLastErrorMessage() << endl;
+        datos.close();
+        return -1;
+    }
+
+    if( !( codigo = control.send( "LIST\r\n", 6, 0 ) ) ) throw codigo;
+    if( !( codigo = recibir( control ) ) ) throw codigo;
+
+    if( recibir( datos, aux ) < 0 ) {
+        cout << datos.getLastErrorMessage() << endl;
+        datos.close();
+        return -1;
+    }
+    datos.close();
+
+    if( !( codigo = recibir( control ) ) ) throw codigo;
+
+    ssListado << aux ;
+    while( getline( ssListado, aux_2 ) ) {
+        istringstream issfila( aux_2 );
+        issfila >> aux >> aux >> aux >> aux >> aux >> aux >> aux >> aux >> nombre;
+
+        listado << padding;
+        if( aux_2.at( 0 ) != 'd' ) {
+            listado << "---> " << nombre << endl;
+        } else {
+            listado << "---[ " << nombre << endl;
+
+            if( profundidad < NIVEL_PROFUNDIDAD ) {
+                if( !( codigo = control.send( "PWD\r\n", 5, 0 ) ) ) throw codigo;
+                if( !( codigo = recibir( control, aux ) ) ) throw codigo;
+                aux.erase( 0, aux.find_first_of( "\"" ) + 1 );
+                aux.erase( aux.find_first_of( "\"" ) );
+
+                aux_2 = C_STR( "CWD " << aux << ( aux.back() == '/' ? "" : "/" ) << nombre << "\r\n" );
+                if( !( codigo = control.send( aux_2.c_str(), aux_2.length(), 0 ) ) ) throw codigo;
+                if( !( codigo = recibir( control ) ) ) throw codigo;
+
+                listarDirectorios( control, profundidad + 1, listado, ( padding +  "   |"  ) );
+
+                if( !( codigo = control.send( "CDUP\r\n", 6, 0 ) ) ) throw codigo;
+                if( !( codigo = recibir( control ) ) ) throw codigo;
+                listado<<padding<<endl;
+            }
+        }
+    }
+    return 0;
 }
 
 bool inputError() {
