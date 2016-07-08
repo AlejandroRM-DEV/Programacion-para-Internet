8f0f935dcf8fd4c109828a554946bd3a8c0b1fa4
Alejandro Ramírez Muñoz <alejandrorm45@yahoo.com>
2016-07-08 13:31:06 -0500

    Act ftp

--- ListadoFTP/main.cpp
+++ ListadoFTP/main.cpp
@@ -14,12 +14,14 @@
 #define TAMANO_BUFFER 512
 #define NIVEL_PROFUNDIDAD 3
 
-using namespace std;
-
-static bool LOG = false;
+#define RESPUESTA_INESPERADA -2
 
 #define C_STR(ITEMS)  ( ( dynamic_cast<ostringstream &> ( ostringstream() . seekp( 0, ios_base::cur ) << ITEMS ) ) \
                        .str().c_str() )
+
+using namespace std;
+
+static bool LOG = true;
 
 template <typename T>
 inline bool str_to_num( const string& str, T &num ) {
@@ -51,17 +53,34 @@ int main() {
     try {
         if( !control.connect( url->host.c_str(), url->puerto.c_str(), &hints ) ) throw - 1;
         if( !( codigo = recibir( control ) ) ) throw codigo;
+        switch( codigo ) {
+        case 120: if( !( codigo = recibir( control ) ) ) throw codigo; break;
+        case 220: break; // Todos bien
+        case 421: throw RESPUESTA_INESPERADA;
+        }
 
         comando = C_STR( "USER " << url->usuario << "\r\n" );
         if( !( codigo = control.send( comando.c_str(), comando.length(), 0 ) ) ) throw codigo;
         if( !( codigo = recibir( control ) ) ) throw codigo;
+        switch( codigo ) {
+        case 530: case 500: case 501: case 421: case 332: throw RESPUESTA_INESPERADA;
+        case 230: case 331: break; // Todos bien
+        }
 
         comando = C_STR( "PASS " << url->contrasena << "\r\n" );
         if( !( codigo = control.send( comando.c_str(), comando.length(), 0 ) ) )throw codigo;
         if( !( codigo = recibir( control ) ) ) throw codigo;
+        switch( codigo ) {
+        case 230: break; // Todos bien
+        case 202: case 530: case 500: case 501: case 503: case 421: case 332: throw RESPUESTA_INESPERADA;
+        }
 
         if( !( codigo = control.send( "CWD /\r\n", 7, 0 ) ) ) throw codigo;
         if( !( codigo = recibir( control ) ) ) throw codigo;
+        switch( codigo ) {
+        case 250: break; // Todos bien
+        case 500: case 501: case 502: case 421: case 530: case 550: throw RESPUESTA_INESPERADA;
+        }
 
         listado << "---| Root" << endl;
         listarDirectorios( control, 0, listado, "   |" );
@@ -69,6 +88,10 @@ int main() {
 
         if( !( codigo = control.send( "QUIT\r\n", 6, 0 ) ) ) throw codigo;
         if( !( codigo = recibir( control ) ) ) throw codigo;
+        switch( codigo ) {
+        case 221: break; // Todos bien
+        case 500: throw RESPUESTA_INESPERADA;
+        }
     } catch( int e ) {
         if( e == -1 ) {
             cout << control.getLastErrorMessage() << endl;
@@ -76,6 +99,9 @@ int main() {
             return -1;
         } else if( e == 0 ) {
             cout << "Conexion cerrada" << endl;
+            control.close();
+        } else if( e == RESPUESTA_INESPERADA ) {
+            cout << "La aplicacion se cerro, no era posible continuar la ejecucion" << endl;
             control.close();
         }
     }
@@ -157,6 +183,10 @@ int listarDirectorios( SocketPortable &control, int profundidad, stringstream &l
 
     if( !( codigo = control.send( "PASV\r\n", 6, 0 ) ) ) throw codigo;
     if( !( codigo = recibir( control, aux ) ) ) throw codigo;
+    switch( codigo ) {
+    case 227: break; // Todos bien
+    case 500: case 501: case 502: case 421: case 530: throw RESPUESTA_INESPERADA;
+    }
     if( LOG ) cout << aux;
 
     inicio = aux.find_first_of( "(", 0 ) + 1;
@@ -188,15 +218,21 @@ int listarDirectorios( SocketPortable &control, int profundidad, stringstream &l
 
     if( !( codigo = control.send( "LIST\r\n", 6, 0 ) ) ) throw codigo;
     if( !( codigo = recibir( control ) ) ) throw codigo;
-
+    switch( codigo ) {
+    case 125: case 150: break;
+    case 450: case 500: case 501: case 502: case 421: case 530: throw RESPUESTA_INESPERADA;
+    }
     if( recibir( datos, aux ) < 0 ) {
         cout << datos.getLastErrorMessage() << endl;
         datos.close();
         return -1;
     }
     datos.close();
-
     if( !( codigo = recibir( control ) ) ) throw codigo;
+    switch( codigo ) {
+    case 226: case 250: break; // Todo bien
+    case 425: case 426: case 421: throw RESPUESTA_INESPERADA;
+    }
 
     ssListado << aux ;
     while( getline( ssListado, aux_2 ) ) {
@@ -205,25 +241,38 @@ int listarDirectorios( SocketPortable &control, int profundidad, stringstream &l
 
         listado << padding;
         if( aux_2.at( 0 ) != 'd' ) {
-            listado << "---> " << nombre << endl;
+            listado << "--->> " << nombre << endl;
         } else {
-            listado << "---[ " << nombre << endl;
+            listado << "---[] " << nombre << endl;
 
             if( profundidad < NIVEL_PROFUNDIDAD ) {
                 if( !( codigo = control.send( "PWD\r\n", 5, 0 ) ) ) throw codigo;
                 if( !( codigo = recibir( control, aux ) ) ) throw codigo;
+                switch( codigo ) {
+                case 257: break; // Todos bien
+                case 500: case 501: case 502: case 421: case 550: throw RESPUESTA_INESPERADA;
+                }
                 aux.erase( 0, aux.find_first_of( "\"" ) + 1 );
                 aux.erase( aux.find_first_of( "\"" ) );
 
                 aux_2 = C_STR( "CWD " << aux << ( aux.back() == '/' ? "" : "/" ) << nombre << "\r\n" );
                 if( !( codigo = control.send( aux_2.c_str(), aux_2.length(), 0 ) ) ) throw codigo;
                 if( !( codigo = recibir( control ) ) ) throw codigo;
+                switch( codigo ) {
+                case 250: break; // Todos bien
+                case 500: case 501: case 502: case 421: case 530: case 550: throw RESPUESTA_INESPERADA;
+                }
 
                 listarDirectorios( control, profundidad + 1, listado, ( padding +  "   |"  ) );
 
                 if( !( codigo = control.send( "CDUP\r\n", 6, 0 ) ) ) throw codigo;
                 if( !( codigo = recibir( control ) ) ) throw codigo;
-                listado<<padding<<endl;
+                switch( codigo ) {
+                case 200: break; // Todos bien
+                case 500: case 501: case 502: case 421: case 530: case 550: throw RESPUESTA_INESPERADA;
+                }
+
+                listado << padding << endl;
             }
         }
     }
